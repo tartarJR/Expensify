f4a7244ef5fd3740a173ee2b8f1d3738605e7e8a
tartarJR <mustafaogun.ozturk@gmail.com>
2019-10-29 13:56:36 +0200

    Refactor ExpenseDao to use BaseDao

--- app/src/main/java/com/tatar/expensify/data/ExpenseDao.kt
+++ app/src/main/java/com/tatar/expensify/data/ExpenseDao.kt
@@ -1,31 +1,20 @@
 package com.tatar.expensify.data
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Query
 
 @Dao
-interface ExpenseDao {
+interface ExpenseDao : BaseDao<Expense> {
 
-    @Query("SELECT * FROM expenses_table ORDER BY _id DESC")
+    @Query("SELECT * FROM expenses_table ORDER BY id DESC")
     fun getAllExpenses(): LiveData<List<Expense>> // TODO nullable?
 
-    @Query("SELECT * FROM expenses_table WHERE _id = :key")
+    @Query("SELECT * FROM expenses_table WHERE id = :key")
     fun get(key: Long): Expense // TODO nullable?, why is it not LiveData
 
-    @Query("SELECT * FROM expenses_table ORDER BY _id DESC LIMIT 1")
+    @Query("SELECT * FROM expenses_table ORDER BY id DESC LIMIT 1")
     fun getLastExpense(): Expense? // TODO nullable?, why is it not LiveData
-
-    @Insert
-    fun insert(expense: Expense)
-
-    @Update
-    fun update(expense: Expense)
-
-    @Delete
-    fun deleteGivenExpense(expense: Expense)
-
-    @Delete
-    fun deleteGivenExpenses(expenses: List<Expense>): Int
 
     @Query("DELETE FROM expenses_table")
     fun clearExpenses()
