cdcf95824dedcb6d072218eb5685d451f7ccb666
tartarJR <mustafaogun.ozturk@gmail.com>
2019-10-29 11:32:16 +0200

    Refactor ExpenseType enum to be an Room Entitiy

--- app/src/main/java/com/tatar/expensify/data/ExpenseType.kt
+++ app/src/main/java/com/tatar/expensify/data/ExpenseType.kt
@@ -1,8 +1,13 @@
 package com.tatar.expensify.data
 
-enum class ExpenseType {
-    FOOD,
-    EAT_OUT,
-    HANG_OUT,
-    OTHER
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "expense_types_table")
+class ExpenseType(
+
+    @PrimaryKey(autoGenerate = true)
+    var _id: Long = 0L,
+    var name: String,
+    var explanation: String
+)
