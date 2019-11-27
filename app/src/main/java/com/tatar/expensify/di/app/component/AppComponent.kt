32c1605a6a0a7c83207e751de438c53adb671ad7
tartarJR <mustafaogun.ozturk@gmail.com>
2019-11-27 16:11:44 +0200

    Add missing modules and dependencies to be provided from AppComponent

--- app/src/main/java/com/tatar/expensify/di/app/component/AppComponent.kt
+++ app/src/main/java/com/tatar/expensify/di/app/component/AppComponent.kt
@@ -1,8 +1,12 @@
 package com.tatar.expensify.di.app.component
 
 import android.app.Application
+import android.content.Context
+import com.tatar.expensify.data.local.expense.ExpenseDao
+import com.tatar.expensify.data.local.expensetype.ExpenseTypeDao
 import com.tatar.expensify.di.app.module.DatabaseModule
 import com.tatar.expensify.di.app.module.GlideModule
+import com.tatar.expensify.di.app.module.NetworkModule
 import com.tatar.expensify.di.app.scope.PerApp
 import dagger.BindsInstance
 import dagger.Component
@@ -12,18 +16,22 @@ import dagger.Component
 @Component(
     modules = [
         DatabaseModule::class,
-        GlideModule::class
+        GlideModule::class,
+        NetworkModule::class
     ]
 )
 interface AppComponent {
 
     // TODO provide app level dependencies here
+    fun expenseDao(): ExpenseDao
+
+    fun expenseTypeDao(): ExpenseTypeDao
 
     @Component.Builder
     interface Builder {
 
         @BindsInstance
-        fun app(application: Application): Builder // Bind the instance of Application to the component at the time of component construction
+        fun app(applicationContext: Context): Builder // Bind the instance of Application to the component at the time of component construction
 
         fun build(): AppComponent // Mandatory for the component
     }
